a804bc3e8f5d29f07104931e41fac963b687e746
Kuri-Tai <kuri@db.ics.keio.ac.jp>
2021-05-07 12:35:43 +0900

    Add a sample format command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const disposable1 = vscode.commands.registerCommand('vscode-ssql.formatOriginalLog', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const selection = editor.selection;
+            const text = document.getText(selection);
+            if (text) {
+                const textFormatted = formatOriginalLog(text);
+                if (textFormatted) {
+                    editor.edit(editBuilder => {
+                        editBuilder.replace(selection, textFormatted);
+                    });
+                } else {
+                    console.log("text not formatted.");
+                }
+            } else {
+                console.log("text not detected.");
+            }
+        }
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ssql" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ssql.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ssql_formatter!');
-	});
-
-	context.subscriptions.push(disposable);
+    const disposable2 = vscode.commands.registerCommand('vscode-ssql.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from vscode-ssql!');
+    });
+    context.subscriptions.push(disposable1);
+    context.subscriptions.push(disposable2);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+// 独自ログ文字列を見やすく整形する例
+function formatOriginalLog(text: string): string {
+    let textFormatted = '';
+    const articles = text.split('|');
+    if (articles.length > 1) {
+        articles.forEach(article => {
+            const fields = article.split(':');
+            if (fields.length > 1) {
+                fields.forEach(field => {
+                    textFormatted += `${field}\n`;
+                });
+                textFormatted += `\n`;
+            }
+        });
+    }
+    return textFormatted;
+}
