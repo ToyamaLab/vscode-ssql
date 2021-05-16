8941d1602d75224629b379a1c5d4bb72303a0e4c
Jun Nemoto <jun.nemoto@gmail.com>
2021-05-16 10:23:12 +0900

    Add in-extension configuration
    Add in-extension configuration when executing SSQL. You can specify
    SSQL jar file, configuration file (.ssql), and custom settings
    that can overwrite .ssql settings.
    
    Also, some global variables have refactored.

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,95 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
+const JAVA_COMMAND = 'java';
+const SSQL_VSCODE_CHANNEL = 'SuperSQL';
+const SSQL_VSCODE_COMMAND = 'ssql.exec';
+const SSQL_CONFIG_NAME = 'ssql';
+const SSQL_CLASS = 'supersql.FrontEnd';
+const SSQL_DEFAULT_JAR_PATH = '/lib/supersql.jar';
+
+function makeSSQLCommand(context: vscode.ExtensionContext) {
+	let jarFile = ' -cp ' + context.extensionPath + SSQL_DEFAULT_JAR_PATH;
+	let configFile = '';
+	let driver = '';
+	let host = '';
+	let port = '';
+	let db = '';
+	let user = '';
+	let password = '';
+	let outputDirectory = '';
+
+	const config: vscode.WorkspaceConfiguration
+		= vscode.workspace.getConfiguration(SSQL_CONFIG_NAME);
+
+	if (config.bringYourOwnJarFile !== '') {
+		jarFile = ' -cp ' + config.bringYourOwnJarFile;
+	}
+
+	if (config.configFile !== '') {
+		configFile = " -c " + config.configFile;
+	}
+
+	if (config.custom.db.driver !== '') {
+		driver = " -driver " + config.custom.db.driver;
+	}
+
+	if (config.custom.db.host !== '') {
+		host = " -h " + config.custom.db.host;
+	}
+
+	if (config.custom.db.hostPort !== '') {
+		port = " -p " + config.custom.db.hostPort;
+	}
+
+	if (config.custom.db.name !== '') {
+		db = " -db " + config.custom.db.name;
+	}
+
+	if (config.custom.db.user !== '') {
+		user = " -u " + config.custom.db.user;
+	}
+
+	if (config.custom.db.userPassword !== '') {
+		password = " -password " + config.custom.db.userPassword;
+	}
+
+	if (config.custom.outputDirectory !== '') {
+		outputDirectory = " -d " + config.custom.outputDirectory;
+	}
+
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return '';
+	}
+	const file = editor.document.fileName;
+
+	const command = JAVA_COMMAND + jarFile
+		+ ' ' + SSQL_CLASS + ' -f ' + file
+		+ configFile + driver + host + port + db + user + password
+		+ outputDirectory + ' ' + config.custom.vastAdditionalOptions;
+
+	return command;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('VScode extension for SuperSQL is now activated.');
 
-	let outputChannel = vscode.window.createOutputChannel("SuperSQL");
-	const command = 'ssql.exec';
-	const ssqlJarPath = context.extensionPath + '/lib/supersql.jar';
+	let outputChannel = vscode.window.createOutputChannel(SSQL_VSCODE_CHANNEL);
 	const commandHandler = () => {
-		let editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			// We heard that this is necessary to avoid warning.
-			// But is it true? Not smart.
-			// https://qiita.com/hakua-doublemoon/items/cfb638fe1ab43ca42cb8
-			return "";
+		const command = makeSSQLCommand(context);
+		if (command !== '') {
+			child_process.exec(command, (error, stdout, stderror) => {
+				outputChannel.appendLine(command);
+				outputChannel.appendLine(stdout);
+				outputChannel.appendLine(stderror);
+				outputChannel.show();
+			});
+		} else {
+			outputChannel.appendLine('Making SuplerSQL command failed.');
 		}
-		let file = editor.document.fileName;
-		child_process.exec('java -cp ' + ssqlJarPath + ' supersql.FrontEnd -f ' + file, (error, stdout, stderror) => {
-            outputChannel.appendLine(stdout);
-            outputChannel.appendLine(stderror);
-			outputChannel.show();
-        });
 	};
-	context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+	context.subscriptions.push(vscode.commands.registerCommand(SSQL_VSCODE_COMMAND, commandHandler));
 }
 
 export function deactivate() {}
